645f1d923930a84c7f308dafc720d555f121698a
xiaogang <maple.hl@gmail.com>
2024-07-29 00:27:21 +0800

    ch 7

---
+++ docs/7-flakes.md
@@ -0,0 +1,9 @@
+# Flakes
+
+Original post [7 what about flakes then?](https://tonyfinn.com/blog/nix-from-first-principles-flake-edition/nix-7-what-about-flakes-then/)
+
+## what is a flake?
+
+## an example flake
+
+./flake.nix
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.default =
+      pkgs.stdenv.mkDerivation {
+        src = ./rust-hello;
+        name = "rust-hello-1.0";
+        nativeBuildInputs = [pkgs.cargo];
+        buildPhase = ''
+          cargo build --release
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/release/rust-hello $out/bin/rust-hello
+          chmod +x $out
+        '';
+      };
+  };
+}
