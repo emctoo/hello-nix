f25975760f9389c75a4919de7f704f00785e2ca2
xiaogang <maple.hl@mail.com>
2024-07-29 11:13:24 +0800

    07 new debug output

--- 07-flakes/flake.nix
+++ 07-flakes/flake.nix
@@ -9,8 +9,11 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    packages.${system}.default =
-      pkgs.stdenv.mkDerivation {
+    # this won't work
+    # packages.${system}.default = ...;
+    # packages.${system}.debug = ...;
+    packages.${system} = {
+      default = pkgs.stdenv.mkDerivation {
         src = ../rust-hello;  # parent dir
         name = "rust-hello-1.0";
         nativeBuildInputs = [pkgs.cargo];
@@ -23,17 +26,18 @@
           chmod +x $out
         '';
       };
-    packages.${system}.debug = pkgs.stdenv.mkDerivation {
-      name = "rust-hello-debug-1.0";
-      nativeBuildInputs = [pkgs.cargo];
-      buildPhase = ''
-        cargo build
-      '';
-      installPhase = ''
-        mkdir -p $out/bin
-        cp target/debug/rust-hello $out/bin/rust-hello
-        chmod +x $out
-      '';
+      debug = pkgs.stdenv.mkDerivation {
+        name = "rust-hello-debug-1.0";
+        nativeBuildInputs = [pkgs.cargo];
+        buildPhase = ''
+          cargo build
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/debug/rust-hello $out/bin/rust-hello
+          chmod +x $out
+        '';
+      };
     };
   };
 }
