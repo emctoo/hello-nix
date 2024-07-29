df5eac3cfa8d8a0185fe9ead81ed4141228ca225
xiaogang <maple.hl@mail.com>
2024-07-29 11:45:21 +0800

    08 build multiple shell with mapAttrs

--- 08-dev-env-with-flakes/flake.nix
+++ 08-dev-env-with-flakes/flake.nix
@@ -5,13 +5,26 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.cargo
-          pkgs.python3
-        ];
-        RUST_LOG = "debug";
+      pythonVersions = {
+        python311 = pkgs.python311;
+        python312 = pkgs.python312;
+        default = pkgs.python312;
       };
+      mkPythonShell = shellName: pythonPackage: pkgs.mkShell {
+        packages = [ pythonPackage ];
+        shellHook = ''
+          echo "Enterning ${shellName} ..."
+        '';
+      };
+    in {
+      # devShells.${system}.default = pkgs.mkShell {
+      #   packages = [
+      #     pkgs.cargo
+      #     pkgs.python3
+      #   ];
+      #   RUST_LOG = "debug";
+      # };
+
+      devShell.${system} = builtins.mapAttrs mkPythonShell pythonVersions;
     };
 }
